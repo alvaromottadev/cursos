c7876b777ad5b8b841497cb0652066019968b9b5
motta <alvaromotta.dev@gmail.com>
2025-10-22 15:06:59 -0300

    feat: implement product validation service with product and validation models, repositories, and service logic

---
+++ microservicos-arquitetura-saga-orquestrado/product-validation-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.productS(id, code) VALUES (1, 'COMIC_BOOKS');
+INSERT INTO public.productS(id, code) VALUES (2, 'BOOKS');
+INSERT INTO public.productS(id, code) VALUES (3, 'MOVIES');
+INSERT INTO public.productS(id, code) VALUES (4, 'MUSIC');
